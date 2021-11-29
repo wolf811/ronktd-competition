680a601e374a3631db82afd2d4ddfd06f22c1db0
popov <popov@naks.ru>
2021-11-29 18:28:12 +0300

    wip form saving and errors

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -3,7 +3,8 @@
 //captcha django-simple-captcha
 //
 $('#refresh_captcha').on('click', function (e) {
-	console.log('refresh clicked...');
+	e.preventDefault();
+	//console.log('refresh clicked...');
 	$.getJSON('/captcha/refresh/', function (result) {
 		$('.captcha').attr('src', result['image_url']);
 		$('#id_captcha_0').val(result['key']);
