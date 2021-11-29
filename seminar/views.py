142bcf271d5661d42bc1f783599216552d637793
popov <popov@naks.ru>
2021-11-29 14:56:46 +0300

    wip forms

--- seminar/views.py
+++ seminar/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from seminar.models import Seminar, STheme
+from seminar.models import SDocument, Seminar, STheme
 
 
 # Create your views here.
@@ -11,12 +11,15 @@ def index(request):
     descriptions = None
     themes = None
     speakers = None
+    theme_documents = None
+    seminar_documents = None
     if seminar:
         banners = seminar.banners.select_related().order_by("number")
         descriptions = seminar.descriptions.select_related().order_by("number")
         speakers = seminar.speakers.select_related().order_by("number")
         themes = STheme.objects.filter(speaker__in=speakers).order_by("number")
-        # __import__("pdb").set_trace()
+        theme_documents = SDocument.objects.filter(theme__in=themes)
+        seminar_documents = SDocument.objects.filter(seminar=seminar)
     content = {
         "title": title,
         "seminar": seminar,
@@ -24,5 +27,7 @@ def index(request):
         "descriptions": descriptions,
         "themes": themes,
         "speakers": speakers,
+        "theme_documents": theme_documents,
+        "seminar_documents": seminar_documents,
     }
     return render(request, "seminar/index.html", content)
