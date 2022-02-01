26c9f3741310a3b010920d20eb9f5d5eb767e84b
testpass1982 <popov@naks.ru>
2022-02-01 15:09:54 +0300

    fix1

--- common/context_processors.py
+++ common/context_processors.py
@@ -5,4 +5,4 @@ def active_event(request):
     active_event = Event.objects.filter(active_now=True).first()
     if active_event:
         return {"glob_event": active_event}
-    return None
+    return ""
